ee580239e4b62f7278f85320f94bfe03e26992fd
Ivan De Marino <ivan.de.marino@gmail.com>
2010-01-14 00:13:33 +0000

    Calculate abs(int) avoiding branching and only using bitwise operations

---
+++ abs_without_branching.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int abs(const int value) {
+   int A = value >> 31;     // 0x00 if Positive, 0xFF if Negative
+   int B = -A;              // 0x00 if Positive, 0x01 if Negative
+   return (value ^ A) + B;  // value ^ A = value if Positive, value ^ A = ~value if Negative
+}
+
+int main(int argc, char** argv)
+{
+    int input;
+    
+    // Check the Input
+	if ( argc == 2 ) input = atoi(argv[1]); else return (1);
+	
+	printf("abs(%d) = %d\n\n", input, abs(input));
+}
