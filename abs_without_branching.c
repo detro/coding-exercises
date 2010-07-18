0906a620dd63162aae4919d18ba4f5be648ab4df
Ivan De Marino <ivan.de.marino@gmail.com>
2010-07-18 22:19:41 +0100

    Fixed a mis-usage of the abs() function

--- abs_without_branching.c
+++ abs_without_branching.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int abs(const int value) {
+int absolute(const int value) {
    int A = value >> 31;     // 0x00 if Positive, 0xFF if Negative
    int B = -A;              // 0x00 if Positive, 0x01 if Negative
+   
    return (value ^ A) + B;  // value ^ A = value if Positive,
                             //    value ^ A = ~value if Negative
    // or just: return (value ^ A) - A;
@@ -14,6 +15,6 @@ int main(int argc, char** argv)
     
     // Check the Input
 	if ( argc == 2 ) input = atoi(argv[1]); else return (1);
-	
-	printf("abs(%d) = %d\n\n", input, abs(input));
+
+	printf("abs(%d) = %d\n\n", input, absolute(input));
 }
