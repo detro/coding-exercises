2115f2807852c05bf2688111ada99249e3ad67a2
Ivan De Marino <ivan.de.marino@gmail.com>
2012-08-04 12:55:57 +0100

    A bit better internal doc for 'abs_without_branching.c'

--- abs_without_branching.c
+++ abs_without_branching.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int absolute(const int value) {
-   int A = value >> 31;     // 0x00 if Positive, 0xFF if Negative
-   int B = -A;              // 0x00 if Positive, 0x01 if Negative
-   
-   return (value ^ A) + B;  // value ^ A = value if Positive,
-                            //    value ^ A = ~value if Negative
+   int A = value >> 31;     // 0x00 == 0 if value was Positive, 0xFF == -1 if value was Negative
+   int B = -A;              // 0x00 == 0 if value was Positive, 0x01 == 1 if value was Negative
+
+   return (value ^ A) + B;  // (value ^ A) = value if value was Positive,
+                            // or (value ^ A) = complement(value) if value was Negative
    // or just: return (value ^ A) - A;
 }
 
 int main(int argc, char** argv)
 {
     int input;
-    
+
     // Check the Input
 	if ( argc == 2 ) input = atoi(argv[1]); else return (1);
 
