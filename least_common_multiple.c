aed1aa27f572bc0b657189d2dc27557a0f1d6c6d
Ivan De Marino <ivan.de.marino@gmail.com>
2012-08-12 14:32:48 +0100

    Moving the 'LCM' calculator to use u_long (and fixed printf use with unsigned values)

--- least_common_multiple.c
+++ least_common_multiple.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <sys/types.h>
 
-u_int least_common_multiple(u_int a, u_int b) {
-    u_int multi_a = a, factor_a = 1;
-    u_int multi_b = b, factor_b = 1;
+u_long least_common_multiple(u_long a, u_long b) {
+    u_long multi_a = a, factor_a = 1;
+    u_long multi_b = b, factor_b = 1;
 
     while(multi_a != multi_b) {
         if (multi_a < multi_b) {
@@ -18,16 +20,16 @@ u_int least_common_multiple(u_int a, u_int b) {
 
 int main(int argc, char** argv)
 {
-    u_int a, b;
+    u_long a, b;
 
     // Check the Input
     if (argc == 3) {
-        a = atoi(argv[1]);
-        b = atoi(argv[2]);
+        a = strtol(argv[1], NULL, 10);
+        b = strtol(argv[2], NULL, 10);
     } else
         return 1;
 
-    printf("Least Common Multiple for '%d' and '%d' is '%d'\n",
+    printf("Least Common Multiple for '%lu' and '%lu' is '%lu'\n",
         a, b,
         least_common_multiple(a, b));
     return 0;
